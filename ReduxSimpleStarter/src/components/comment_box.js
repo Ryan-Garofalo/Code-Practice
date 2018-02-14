425b5fe8939b77ab315d19a4b1a59f773f769db2
Ryan Garofalo <ryangarofalo100@gmail.com>
2018-02-14 16:01:22 -0700

    wrote some more test

--- ReduxSimpleStarter/src/components/comment_box.js
+++ ReduxSimpleStarter/src/components/comment_box.js
@@ -1,36 +1,42 @@
 import React, { Component } from "react";
+import {connect} from 'react-redux';
+import * as actions from '../actions';
+
 
 class CommentBox extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
-        this.state ={ comment:''};
+        this.state = { comment: "" };
     }
 
-
-    handleChange(e){
-        this.setState({comment: e.target.value});
+    handleChange(e) {
+        this.setState({ comment: e.target.value });
     }
 
-    handleSubmit(e){
+    handleSubmit(e) {
         e.preventDefault();
+        this.props.saveComment(this.state.comment);
 
-        this.setState({comment:''})
+        this.setState({ comment: "" });
     }
-
 
     render() {
         return (
             <form
                 onSubmit={this.handleSubmit.bind(this)}
-                className='comment-box'>
-				<textarea
+                className="comment-box"
+            >
+                <textarea
                     value={this.state.comment}
-                    onChange={this.handleChange.bind(this)}></textarea>
+                    onChange={this.handleChange.bind(this)}
+                />
                 <button>Submit Comment</button>
             </form>
         );
     }
 }
 
-export default CommentBox;
+
+
+export default connect(null, actions)(CommentBox);
