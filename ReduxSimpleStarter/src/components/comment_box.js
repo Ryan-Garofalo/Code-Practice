ba6c75db3b2a596a73b5814dc6c263fb649bc0e9
Ryan Garofalo <ryangarofalo100@gmail.com>
2018-02-14 12:57:28 -0700

    created a few more test

--- ReduxSimpleStarter/src/components/comment_box.js
+++ ReduxSimpleStarter/src/components/comment_box.js
@@ -1,12 +1,34 @@
 import React, { Component } from "react";
 
 class CommentBox extends Component {
+    constructor(props){
+        super(props);
+
+        this.state ={ comment:''};
+    }
+
+
+    handleChange(e){
+        this.setState({comment: e.target.value});
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+
+        this.setState({comment:''})
+    }
+
+
     render() {
         return (
-            <div className='comment-box'>
-				<textarea></textarea>
+            <form
+                onSubmit={this.handleSubmit.bind(this)}
+                className='comment-box'>
+				<textarea
+                    value={this.state.comment}
+                    onChange={this.handleChange.bind(this)}></textarea>
                 <button>Submit Comment</button>
-            </div>
+            </form>
         );
     }
 }
--- ReduxSimpleStarter/test/components/app_test.js
+++ ReduxSimpleStarter/test/components/app_test.js
@@ -4,15 +4,20 @@ import App from "../../src/components/app";
 // use describe to group together similar tests
 
 describe("App", () => {
+    let component;
+
+    beforeEach(() => {
+        //create an instance of app
+        component = renderComponent(App);
+    });
 
     // use it to test a single attribute of a target
-    it("shows the correct text", () => {
-        //create an instance of app
-        const component = renderComponent(App);
-
+    it("shows a comment box", () => {
         //Use 'expect' to make an 'assertion' about a target
-        expect(component).to.contain('React simple starter');
+        expect(component.find(".comment-box")).to.exist;
+    });
 
-
+    it("shows a comment list", () => {
+        expect(component.find(".comment-list")).to.exist;
     });
 });
