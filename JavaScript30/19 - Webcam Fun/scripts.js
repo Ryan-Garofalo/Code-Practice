866242c82f281dadefe2fa885462378948168a18
Ryan Garofalo <ryangarofalo100@gmail.com>
2018-03-06 23:53:01 -0700

    halfway thru unreal webcam fun

--- JavaScript30/19 - Webcam Fun/scripts.js
+++ JavaScript30/19 - Webcam Fun/scripts.js
@@ -3,3 +3,43 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+function getVideo(){
+	navigator.mediaDevices.getUserMedia({video: true, audio: false})
+	.then(localMediaStream => {
+		video.src = window.URL.createObjectURL(localMediaStream);
+		video.play();
+
+	})
+	.catch(err =>{
+		console.log( "ooooo nooooo",err);
+	});
+}
+
+
+function paintToCanvas(){
+	const width = video.videoWidth;
+	const height = video.videoHeight;
+	console.log(width, height);
+	canvas.width = width;
+	canvas.height= height
+
+	return setInterval(()=>{
+		ctx.drawImage(video, 0, 0, width, height)
+	}, 16)
+}
+
+function takePhoto(){
+	snap.currentTime = 0 ;
+	snap.play();
+
+	const data = canvas.toDataURL('img/jpeg');
+	console.log(data);
+
+}
+
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas)
