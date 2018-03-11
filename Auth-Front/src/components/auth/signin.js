4b0495358cc72cf2702d7a078b93406b05bb6495
Ryan Garofalo <ryangarofalo100@gmail.com>
2018-03-10 18:25:01 -0700

    fixed bug with old react form

--- Auth-Front/src/components/auth/signin.js
+++ Auth-Front/src/components/auth/signin.js
@@ -1,27 +1,46 @@
-import React, { Component } from "react";
-import { reduxForm } from "redux-form";
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
 
-class Signin extends Component {
+class SignIn extends Component {
+    onSubmit({ email, password }) {
+        console.log(email);
+        console.log(password);
+    }
+
+    renderField(field) {
+        return (
+            <fieldset className="form-group">
+                <label>{field.label}:</label>
+                <input
+                    className="form-control"
+                    type={field.type}
+                    {...field.input}
+                />
+            </fieldset>
+        );
+    }
+
     render() {
         return (
-            <form>
-                <fieldset className="form-group">
-                    <label>Email:</label>
-                    <input className="form-control" />
-                </fieldset>
-                <fieldset>
-                    <label>Password:</label>
-                    <input className="form-control" />
-                </fieldset>
-                <button action="submit" className="btn btn-primary">
-                    Sign in
-                </button>
+            <form onSubmit={this.props.handleSubmit(this.onSubmit.bind(this))}>
+                <Field
+                    name="email"
+                    label="Email"
+                    type="text"
+                    component={this.renderField}
+                />
+                <Field
+                    name="password"
+                    label="Password"
+                    type="password"
+                    component={this.renderField}
+                />
+                <button action="submit" className="btn btn-primary">Sign in</button>
             </form>
         );
     }
 }
 
 export default reduxForm({
-    form: "signin",
-    fields: ["email", "password"]
-})(Signin);
+    form: 'signin',
+})(SignIn);
