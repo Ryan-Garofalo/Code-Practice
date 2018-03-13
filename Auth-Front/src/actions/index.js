2b9f34f215aae865bff76e96886439bacb1ab853
Ryan Garofalo <ryangarofalo100@gmail.com>
2018-03-13 14:31:41 -0600

    singout route complete

--- Auth-Front/src/actions/index.js
+++ Auth-Front/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { browserHistory } from "react-router";
-import { AUTH_USER, AUTH_ERROR } from "./types";
+import { AUTH_USER, AUTH_ERROR, UNAUTH_USER } from "./types";
 
 const ROOT_URL = "http://localhost:3090";
 
@@ -14,23 +14,28 @@ export function signinUser({ email, password }) {
                 // - Update state to indicate user is authenticated
                 dispatch({ type: AUTH_USER });
                 // - Save the JWT token
-                localStorage.setItem('token', response.data.token);
+                localStorage.setItem("token", response.data.token);
                 // - Redirect to the route '/feature'
                 browserHistory.push("/feature");
             })
-            .catch(()=>{
-
-                    // If request is bad ..
-                    // -Show an error to the user
-                    dispatch(authError('Bad'));
+            .catch(() => {
+                // If request is bad ..
+                // -Show an error to the user
+                dispatch(authError("Bad"));
             });
-
     };
 }
 
-export function authError(error){
+export function authError(error) {
     return {
         type: AUTH_ERROR,
-        payload:error
+        payload: error
+    };
+}
+
+export function signoutUser() {
+    localStorage.removeItem("token");
+    return {
+        type: UNAUTH_USER
     };
 }
